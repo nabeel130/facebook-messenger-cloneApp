cd143a269e7c2819d836b480383912750ff8550d
Mohammed Nabeel km <nabeelkmvkd07@gmail.com>
2020-10-13 19:13:15 +0530

    initial commit , setup the form for messaging

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [input , setInput] = useState('');
+  const [messages , setMessages] = useState(['helo bros','hey honey','hey machaanz']);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event => {
+    event.preventDefault();
+  setMessages([...messages , input]);
+  setInput('');
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello react lovers..</h1>
+      <form>
+          <input value={input} onChange={event => setInput(event.target.value)}/>
+          <button type='submit' onClick={sendMessage}>Send message</button>
+      </form>
+
+      {messages.map(message => (
+        <p>{message}</p>
+      ))
+      }
     </div>
   );
 }
